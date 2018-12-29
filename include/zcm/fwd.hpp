c142166d6780a90969269d9616f33ca1ce2b1369
cat <cat@wolfgirl.org>
2018-12-29 21:17:42 +0500

    Add forward declarations header.

---
+++ include/zcm/fwd.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace zcm {
+
+    struct bvec2;
+    struct bvec3;
+    struct bvec4;
+
+    struct vec2;
+    struct vec3;
+    struct vec4;
+
+    struct mat2;
+    struct mat3;
+    struct mat4;
+
+    struct quat;
+
+}
