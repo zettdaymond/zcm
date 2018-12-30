8af4bdc78fb1c5bff006b1c46f8423dfd7fb772e
cat <cat@wolfgirl.org>
2018-12-30 19:06:09 +0500

    Add float constants.

---
+++ include/zcm/constants.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+namespace zcm {
+
+    /// Return the float epsilon.
+    float epsilon() noexcept;
+
+    /// Return 0.
+    float zero() noexcept;
+
+    /// Return 1.
+    float one() noexcept;
+
+    /// Return the pi constant.
+    float pi() noexcept;
+
+    /// Return pi * 2.
+    float two_pi() noexcept;
+
+    /// Return square root of pi.
+    float root_pi() noexcept;
+
+    /// Return pi / 2.
+    float half_pi() noexcept;
+
+    /// Return pi / 2 * 3.
+    float three_over_two_pi() noexcept;
+
+    /// Return pi / 4.
+    float quarter_pi() noexcept;
+
+    /// Return 1 / pi.
+    float one_over_pi() noexcept;
+
+    /// Return 1 / (pi * 2).
+    float one_over_two_pi() noexcept;
+
+    /// Return 2 / pi.
+    float two_over_pi() noexcept;
+
+    /// Return 4 / pi.
+    float four_over_pi() noexcept;
+
+    /// Return 2 / sqrt(pi).
+    float two_over_root_pi() noexcept;
+
+    /// Return 1 / sqrt(2).
+    float one_over_root_two() noexcept;
+
+    /// Return sqrt(pi / 2).
+    float root_half_pi() noexcept;
+
+    /// Return sqrt(2 * pi).
+    float root_two_pi() noexcept;
+
+    /// Return sqrt(ln(4)).
+    float root_ln_four() noexcept;
+
+    /// Return e constant.
+    float e() noexcept;
+
+    /// Return Euler's constant.
+    float euler() noexcept;
+
+    /// Return sqrt(2).
+    float root_two() noexcept;
+
+    /// Return sqrt(3).
+    float root_three() noexcept;
+
+    /// Return sqrt(5).
+    float root_five() noexcept;
+
+    /// Return ln(2).
+    float ln_two() noexcept;
+
+    /// Return ln(10).
+    float ln_ten() noexcept;
+
+    /// Return ln(ln(2)).
+    float ln_ln_two() noexcept;
+
+    /// Return 1 / 3.
+    float third() noexcept;
+
+    /// Return 2 / 3.
+    float two_thirds() noexcept;
+
+    /// Return the golden ratio constant.
+    float golden_ratio() noexcept;
+
+}
---
+++ src/constants.cpp
@@ -0,0 +1,147 @@
+#include <zcm/constants.hpp>
+#include <limits>
+
+float zcm::epsilon() noexcept
+{
+    return std::numeric_limits<float>::epsilon();
+}
+
+float zcm::zero() noexcept
+{
+    return 0.0f;
+}
+
+float zcm::one() noexcept
+{
+    return 1.0f;
+}
+
+float zcm::pi() noexcept
+{
+    return 3.14159265358979323846264338327950288;
+}
+
+float zcm::two_pi() noexcept
+{
+    return 6.28318530717958647692528676655900576;
+}
+
+float zcm::root_pi() noexcept
+{
+    return 1.772453850905516027;
+}
+
+float zcm::half_pi() noexcept
+{
+    return 1.57079632679489661923132169163975144;
+}
+
+float zcm::three_over_two_pi() noexcept
+{
+    return 4.71238898038468985769396507491925432;
+}
+
+float zcm::quarter_pi() noexcept
+{
+    return 0.785398163397448309615660845819875721;
+}
+
+float zcm::one_over_pi() noexcept
+{
+    return 0.318309886183790671537767526745028724;
+}
+
+float zcm::one_over_two_pi() noexcept
+{
+    return 0.159154943091895335768883763372514362;
+}
+
+float zcm::two_over_pi() noexcept
+{
+    return 0.636619772367581343075535053490057448;
+}
+
+float zcm::four_over_pi() noexcept
+{
+    return 1.273239544735162686151070106980114898;
+}
+
+float zcm::two_over_root_pi() noexcept
+{
+    return 1.12837916709551257389615890312154517;
+}
+
+float zcm::one_over_root_two() noexcept
+{
+    return 0.707106781186547524400844362104849039;
+}
+
+float zcm::root_half_pi() noexcept
+{
+    return 1.253314137315500251;
+}
+
+float zcm::root_two_pi() noexcept
+{
+    return 2.506628274631000502;
+}
+
+float zcm::root_ln_four() noexcept
+{
+    return 1.17741002251547469;
+}
+
+float zcm::e() noexcept
+{
+    return 2.71828182845904523536;
+}
+
+float zcm::euler() noexcept
+{
+    return 0.577215664901532860606;
+}
+
+float zcm::root_two() noexcept
+{
+    return 1.41421356237309504880168872420969808;
+}
+
+float zcm::root_three() noexcept
+{
+    return 1.73205080756887729352744634150587236;
+}
+
+float zcm::root_five() noexcept
+{
+    return 2.23606797749978969640917366873127623;
+}
+
+float zcm::ln_two() noexcept
+{
+    return 0.693147180559945309417232121458176568;
+}
+
+float zcm::ln_ten() noexcept
+{
+    return 2.30258509299404568401799145468436421;
+}
+
+float zcm::ln_ln_two() noexcept
+{
+    return -0.3665129205816643;
+}
+
+float zcm::third() noexcept
+{
+    return 0.3333333333333333333333333333333333333333;
+}
+
+float zcm::two_thirds() noexcept
+{
+    return 0.666666666666666666666666666666666666667;
+}
+
+float zcm::golden_ratio() noexcept
+{
+    return 1.61803398874989484820458683436563811;
+}
